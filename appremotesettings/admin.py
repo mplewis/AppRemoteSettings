66e8808ad766e9bc7dc1f1ba7572897743f5c2f1
Matthew Lewis <matt@mplewis.com>
2016-03-24 00:46:47 -0500

    admin: inline app settings

--- appremotesettings/admin.py
+++ appremotesettings/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline, ModelAdmin
 
 from .models import App, Identifier, Key
 
 
-admin.site.register(App)
+class IdentifierInline(TabularInline):
+    model = Identifier
+    extra = 1
+    fields = ('desc', 'value')
+
+
+class KeyInline(TabularInline):
+    model = Key
+    extra = 1
+    fields = ('datatype', 'key', 'value', 'desc')
+
+
+class AppAdmin(ModelAdmin):
+    inlines = [IdentifierInline, KeyInline]
+
+
+admin.site.register(App, AppAdmin)
 admin.site.register(Identifier)
 admin.site.register(Key)
