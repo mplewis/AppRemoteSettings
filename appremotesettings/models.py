f283f2361f3ee9a4607392e5a9e9e96eddff802d
Matthew Lewis <matt@mplewis.com>
2016-03-24 00:37:37 -0500

    admin: prettify names and str

--- appremotesettings/models.py
+++ appremotesettings/models.py
@@ -15,18 +15,27 @@ DATATYPES = (
 
 class App(Model):
     name = CharField(max_length=255)
-    desc = CharField(max_length=255)
+    desc = CharField(max_length=255, verbose_name='description')
+
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.desc)
 
 
 class Identifier(Model):
     app = ForeignKey(App)
-    desc = CharField(max_length=255)
+    desc = CharField(max_length=255, verbose_name='description')
     value = CharField(max_length=255)
+
+    def __str__(self):
+        return '{} - {} ({})'.format(self.app.name, self.desc, self.value)
 
 
 class Key(Model):
     app = ForeignKey(App)
-    desc = CharField(max_length=255)
+    desc = CharField(max_length=255, verbose_name='description')
     key = CharField(max_length=255)
     value = CharField(max_length=255)
-    datatype = IntegerField(choices=DATATYPES, default=DATATYPE_BOOL)
+    datatype = IntegerField(choices=DATATYPES, default=DATATYPE_BOOL, verbose_name='data type')
+
+    def __str__(self):
+        return '{} - {} ({}: {})'.format(self.app.name, self.desc, self.key, self.value)
