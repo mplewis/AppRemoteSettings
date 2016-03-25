0f2670d68eddb150c6a064bf59e23d713346ec01
Matthew Lewis <matt@mplewis.com>
2016-03-25 00:03:56 -0500

    view: more implementation and tests

--- appremotesettings/tests/test_api.py
+++ appremotesettings/tests/test_api.py
@@ -1,17 +1,35 @@
+import json
+
 from django.test import Client
 
+import pytest
 import sure
+
+
+def post_json(client, url, data, *args, **kwargs):
+    return client.post(url, *args, data=json.dumps(data),
+                       content_type='application/json', **kwargs)
 
 
 def test_v1_bad_method():
     c = Client()
-    resp = c.get('/api/v1/',)
+    resp = c.get('/api/v1/')
     resp.status_code.should.eql(405)
 
 
+@pytest.mark.django_db
+def test_v1_no_json():
+    c = Client()
+    resp = c.post('/api/v1/', data={'dummy data': 'this is not json'})
+    resp.status_code.should.eql(400)
+    resp.content.decode().should.contain('Could not parse JSON')
+
+
+@pytest.mark.django_db
 def test_v1_no_app():
     data = {'app_id': 'com.mplewis.myapp'}
     c = Client()
-    resp = c.post('/api/v1/', data)
+    resp = post_json(c, '/api/v1/', data)
     resp.status_code.should.eql(400)
-    resp.body.should.contain('No app found')
+    json.loads(resp.content.decode()).should.eql(
+        {'error': 'No app found with identifier "com.mplewis.myapp"'})
--- appremotesettings/views.py
+++ appremotesettings/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse, Http404, HttpResponseNotAllowed
+from django.http import HttpResponse, Http404, HttpResponseNotAllowed, HttpResponseBadRequest
 
 from appremotesettings.models import Identifier
+
+import json
+
+
+def jsonify(response, data):
+    return response(content=json.dumps(data))
+
+
+def error(message):
+    return {'error': message}
+
+
+def bad_request(message):
+    return jsonify(HttpResponseBadRequest, error(message))
 
 
 def select_api_version(request, api_version):
@@ -12,4 +26,20 @@ def select_api_version(request, api_version):
 def api_v1(request):
     if request.method != 'POST':
         return HttpResponseNotAllowed(['POST'], content='This endpoint only accepts POST')
-    return HttpResponse('POST')
+
+    try:
+        query = json.loads(request.body.decode())
+    except json.decoder.JSONDecodeError:
+        return bad_request('Could not parse JSON')
+
+    try:
+        ident_raw = query['app_id']
+    except KeyError:
+        return bad_request('Key app_id was not present')
+
+    ident = Identifier.objects.filter(value=ident_raw)
+    if not ident:
+        return bad_request('No app found with identifier "{}"'.format(ident_raw))
+
+    app = ident[0].app
+    return app.name
