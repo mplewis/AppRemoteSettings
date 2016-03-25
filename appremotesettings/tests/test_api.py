003f417b795ccc22b69241ef9250d95058a87d24
Matthew Lewis <matt@mplewis.com>
2016-03-25 00:19:34 -0500

    view: fully working app returning keys

--- appremotesettings/tests/test_api.py
+++ appremotesettings/tests/test_api.py
@@ -1,9 +1,12 @@
-import json
+from appremotesettings import models
+from appremotesettings.models import App, Key, Identifier
 
 from django.test import Client
 
 import pytest
 import sure
+
+import json
 
 
 def post_json(client, url, data, *args, **kwargs):
@@ -33,3 +36,27 @@ def test_v1_no_app():
     resp.status_code.should.eql(400)
     json.loads(resp.content.decode()).should.eql(
         {'error': 'No app found with identifier "com.mplewis.myapp"'})
+
+
+@pytest.mark.django_db
+def test_v1_app():
+    app = App(name='MyApp')
+    app.save()
+    ident = Identifier(value='com.mplewis.myapp', app=app)
+    ident.save()
+    keys = [
+        Key(app=app, key='BOOL_SETTING', value='true', datatype=models.DATATYPE_BOOL),
+        Key(app=app, key='INT_SETTING', value='42', datatype=models.DATATYPE_INT),
+        Key(app=app, key='STRING_SETTING', value='yarn', datatype=models.DATATYPE_STRING),
+    ]
+    for key in keys:
+        key.save()
+
+    data = {'app_id': 'com.mplewis.myapp'}
+    c = Client()
+    resp = post_json(c, '/api/v1/', data)
+    resp.status_code.should.eql(200)
+    json.loads(resp.content.decode()).should.eql(
+        {'BOOL_SETTING': True,
+         'INT_SETTING': 42,
+         'STRING_SETTING': 'yarn'})
--- appremotesettings/views.py
+++ appremotesettings/views.py
@@ -1,31 +1,39 @@
-from django.http import HttpResponse, Http404, HttpResponseNotAllowed, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed, HttpResponseBadRequest
 
 from appremotesettings.models import Identifier
 
 import json
 
 
-def jsonify(response, data):
-    return response(content=json.dumps(data))
+def jsonify(data, *args, response=HttpResponse, **kwargs):
+    return response(*args, content=json.dumps(data), **kwargs)
 
 
 def error(message):
     return {'error': message}
 
 
-def bad_request(message):
-    return jsonify(HttpResponseBadRequest, error(message))
+def not_found(message, *args, **kwargs):
+    return jsonify(error(message), *args, response=HttpResponseNotFound, **kwargs)
+
+
+def bad_request(message, *args, **kwargs):
+    return jsonify(error(message), *args, response=HttpResponseBadRequest, **kwargs)
+
+
+def not_allowed(message, *args, **kwargs):
+    return jsonify(error(message), *args, response=HttpResponseNotAllowed, **kwargs)
 
 
 def select_api_version(request, api_version):
     if api_version == '1':
         return api_v1(request)
-    raise Http404('Invalid API version: "{}"'.format(api_version))
+    return not_found('Invalid API version: "{}"'.format(api_version))
 
 
 def api_v1(request):
     if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'], content='This endpoint only accepts POST')
+        return not_allowed('This endpoint only accepts POST', ['POST'])
 
     try:
         query = json.loads(request.body.decode())
@@ -42,4 +50,4 @@ def api_v1(request):
         return bad_request('No app found with identifier "{}"'.format(ident_raw))
 
     app = ident[0].app
-    return app.name
+    return jsonify(app.typed_keys())
